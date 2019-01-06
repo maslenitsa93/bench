ad6ad5c197221febaa3c7708bf57b19a8f4c85d7
maslenitsa93 <maslenitsa93@gmail.com>
2019-01-06 19:44:55 +0300

    Count range from 100 to 1000

--- main.cpp
+++ main.cpp
@@ -30,90 +30,106 @@ pt::ptree boost_tree;
 
 // Add
 
-static void BM_GoogleTreeAdd100(benchmark::State& state) {
+auto BM_GoogleTreeAdd = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             foo f;
             f.bar = i;
             google_tree[std::to_string(i)] = f;
         }
     }
-}
-BENCHMARK(BM_GoogleTreeAdd100);
+};
 
-static void BM_BoostTreeAdd100(benchmark::State& state) {
+auto BM_BoostTreeAdd = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             foo f;
             f.bar = i;
             boost_tree.put(std::to_string(i), f);
         }
     }
-}
-BENCHMARK(BM_BoostTreeAdd100);
+};
 
 // Upsert
 
-static void BM_GoogleTreeUpsert100(benchmark::State& state) {
+auto BM_GoogleTreeUpsert = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             foo f;
             f.bar = i * 10;
             google_tree[std::to_string(i)] = f;
         }
     }
-}
-BENCHMARK(BM_GoogleTreeUpsert100);
+};
 
-static void BM_BoostTreeUpsert100(benchmark::State& state) {
+auto BM_BoostTreeUpsert = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             foo f;
             f.bar = i * 10;
             boost_tree.put(std::to_string(i), f);
         }
     }
-}
-BENCHMARK(BM_BoostTreeUpsert100);
+};
 
 // Lookup
 
-static void BM_GoogleTreeLookup100(benchmark::State& state) {
+auto BM_GoogleTreeLookup = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             google_tree.find(std::to_string(i));
         }
     }
-}
-BENCHMARK(BM_GoogleTreeLookup100);
+};
 
-static void BM_BoostTreeLookup100(benchmark::State& state) {
+auto BM_BoostTreeLookup = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             boost_tree.find(std::to_string(i));
         }
     }
-}
-BENCHMARK(BM_BoostTreeLookup100);
+};
 
 // Remove
 
-static void BM_GoogleTreeRemove100(benchmark::State& state) {
+auto BM_GoogleTreeRemove = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             google_tree.erase(std::to_string(i));
         }
     }
-}
-BENCHMARK(BM_GoogleTreeRemove100);
+};
 
-static void BM_BoostTreeRemove100(benchmark::State& state) {
+auto BM_BoostTreeRemove = [](benchmark::State& state, auto count) {
     for (auto _ : state) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= count; i++) {
             boost_tree.erase(std::to_string(i));
         }
     }
-}
-BENCHMARK(BM_BoostTreeRemove100);
+};
 
-BENCHMARK_MAIN();
+int main(int argc, char** argv) {
+    for (auto i = 100; i <= 1000; i+=100) {
+        benchmark::RegisterBenchmark(std::string("BM_GoogleTreeAdd_").append(std::to_string(i)).c_str(), BM_GoogleTreeAdd, i);
+        benchmark::RegisterBenchmark(std::string("BM_BoostTreeAdd_").append(std::to_string(i)).c_str(), BM_BoostTreeAdd, i);
+    }
+
+    for (auto i = 100; i <= 1000; i+=100) {
+        benchmark::RegisterBenchmark(std::string("BM_GoogleTreeUpsert_").append(std::to_string(i)).c_str(), BM_GoogleTreeUpsert, i);
+        benchmark::RegisterBenchmark(std::string("BM_BoostTreeUpsert_").append(std::to_string(i)).c_str(), BM_BoostTreeUpsert, i);
+    }
+
+    for (auto i = 100; i <= 1000; i+=100) {
+        benchmark::RegisterBenchmark(std::string("BM_GoogleTreeLookup_").append(std::to_string(i)).c_str(), BM_GoogleTreeLookup, i);
+        benchmark::RegisterBenchmark(std::string("BM_BoostTreeLookup_").append(std::to_string(i)).c_str(), BM_BoostTreeLookup, i);
+    }
+
+    for (auto i = 100; i <= 1000; i+=100) {
+        benchmark::RegisterBenchmark(std::string("BM_GoogleTreeRemove_").append(std::to_string(i)).c_str(), BM_GoogleTreeRemove, i);
+        benchmark::RegisterBenchmark(std::string("BM_BoostTreeRemove_").append(std::to_string(i)).c_str(), BM_BoostTreeRemove, i);
+    }
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    return 0;
+}
